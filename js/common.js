346d095d030665e1b43aec187c896a482620eb99
조은혜 <masimel33@gmail.com>
2021-06-03 22:25:41 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,21 @@
+// 상단 통합 검색 
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+//올해 년도 가져오기
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
